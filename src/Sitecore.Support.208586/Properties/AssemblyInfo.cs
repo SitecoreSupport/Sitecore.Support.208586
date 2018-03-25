e9ac5f355fc364e127f33479038a2b6cbc78b394
Muhammad Yasir Nawaz <myn@sitecore..net>
2018-03-25 08:23:22 +0800

    commit

---
+++ src/Sitecore.Support.208586/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.208586")]
+[assembly: AssemblyProduct("Sitecore.Support.208586")]
+[assembly: ComVisible(false)]
